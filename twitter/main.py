0020b6059d6cbe4e01111237b27b64d5f5b078b4
jiphan <jiphan@umich.edu>
2022-04-13 18:30:07 -0500

    lookup, send, delete

--- twitter/main.py
+++ twitter/main.py
@@ -1,42 +1,66 @@
 import tweepy
+import traceback
 import config
 import os
 script_dir = os.path.dirname(__file__)
 args = config.read_yaml(script_dir + '/config.yaml')
 
 api = tweepy.API(tweepy.OAuth2BearerHandler(args['bearer_token']))
-client = tweepy.Client(args['bearer_token'])
-# client = tweepy.Client(
-#     consumer_key=args['consumer_key'],
-#     consumer_secret=args['consumer_secret'],
-#     access_token=args['access_token'],
-#     access_token_secret=args['access_token_secret']
-# )
+# client = tweepy.Client(args['bearer_token'])
+client = tweepy.Client(
+    consumer_key=args["consumer_key"],
+    consumer_secret=args["consumer_secret"],
+    access_token=args["access_token"],
+    access_token_secret=args["access_token_secret"]
+)
 
 
-def main():
-    tweets = [1326355021093498880, 1513653124019494919]
+def lookup(tweets):
     try:
         res = client.get_tweets(
             tweets,
             expansions=["author_id", "attachments.media_keys",
                         "referenced_tweets.id"],
             media_fields=["preview_image_url", "url"],
-            # tweet_fields=["created_at"],
+            tweet_fields=["created_at"],
+            user_auth=True
         )
 
-        for t in res.includes['media']:
-            print(t.url)
-        print()
+        return [
+            list(map(lambda x: x.text, res.data)),
+            list(map(lambda x: x.url, res.includes['media']))
+        ]
+    except:
+        traceback.print_exc()
+        return []
 
-        for t in res.data:
-            print(t.text)
-        print()
 
-        print(res)
+def send(tweet):
+    try:
+        res = client.create_tweet(
+            text=tweet
+        )
+        print(res.data)
+        return res
+    except:
+        traceback.print_exc()
 
-    except Exception as e:
-        print('something happened:', e)
+
+def delete(tweet):
+    try:
+        res = client.delete_tweet(
+            id=tweet
+        )
+        print(res.data)
+    except:
+        traceback.print_exc()
+
+
+def main():
+    tweets = [1326355021093498880, 1514110244578029569]
+    print(lookup(tweets))
+    # t = send('test').data
+    # delete(t['id'])
 
 
 if __name__ == '__main__':
