8faf6b6dc673d938e7844182a015b633a573e1c2
jiphan <jiphan@umich.edu>
2022-07-19 23:08:13 -0500

    refactored

--- yotsuba/calcActivity.py
+++ yotsuba/calcActivity.py
@@ -1,59 +1,61 @@
+from concurrent.futures import thread
 from datetime import datetime
 import time
 import re
 import yotsuba
-bucket = {}
+import config
+import os
+script_dir = os.path.dirname(__file__)
+args = config.read_yaml(script_dir + '/config.yaml')
 
 
-def getBacklinks(thread):
-    postSet = set()
-    res = yotsuba.getThread(thread)
-    bucketThread(res, bucket)
-    op = res[0]
-    if (not re.search(yotsuba.args.threadRegex, op['com'])):
-        print('skipping')
-        return postSet
-    for post in res:
-        if post.get('com') and 'quotelink' in post['com']:
-            clean = yotsuba.escapeHtml(post['com'])
-            link = re.findall(r'(?<=>>)(\d+)', clean)
-            postSet.update(map(int, link))
-    return postSet
+def bucketTime(timestamp):
+    hours = timestamp.strftime('%H:%M')[:-2]
+    minutes = int(timestamp.strftime('%H:%M')[-2:]) // 15 * 15
+    minutes = str(minutes).ljust(2, '0')
+    return hours + minutes
 
 
-def getRelatedThreads(threadList):
-    arr = set(yotsuba.getArchive())
-    threadQueue = set()
-    threadQueue.add(threadList[0])
-    while (len(threadQueue) > 0):
-        # print(threadQueue)
-        cur = threadQueue.pop()
-        curLinks = getBacklinks(cur)
-        if len(curLinks) == 0:
-            print('removing')
-            threadList.remove(cur)
-        next = arr & curLinks
-        for i in next:
-            if i not in threadList:
-                threadQueue.add(i)
-                threadList.append(i)
-        time.sleep(1)
-
-        if len(threadList) > 5:
-            break
-    return threadList
-
-
-def bucketThread(data, bucket):
-    for i in data:
-        dt = datetime.fromtimestamp(i['time'])
-        q = int(dt.strftime('%H:%M')[-2:]) // 15 * 15
-        dt = dt.strftime('%H:%M')[:-2] + str(q).ljust(2, '0')
-        bucket[dt] = 1 if dt not in bucket else bucket[dt] + 1
+def getCurrent():
+    res = set()
+    for page in yotsuba.getCatalogFull(args.board):
+        for thread in page['threads']:
+            if re.search(args.threadRegex, thread.get('com', '')):
+                res.add(thread['no'])
+    return res
 
 
 if __name__ == '__main__':
-    print(yotsuba.args)
-    q = yotsuba.getCatalog()
-    q = getRelatedThreads(q)
-    [print(i, str(bucket[i])) for i in sorted(bucket)]
+    print(args)
+    expired = set(yotsuba.getArchive(args.board))
+    threadQueue = getCurrent()
+    allThreads = getCurrent()
+    # allThreads = {getCurrent()}
+    bucket = {}
+
+    while (len(threadQueue) > 0):
+        print(threadQueue, allThreads)
+        # pull current thread in queue
+        cur = yotsuba.getThread(args.board, threadQueue.pop())
+        if (not re.search(args.threadRegex, cur[0]['com'])):
+            continue
+
+        # bucket timestamps to 15min intervals
+        for i in cur:
+            dt = bucketTime(datetime.fromtimestamp(i['time']))
+            bucket[dt] = bucket[dt] + 1 if dt in bucket else 1
+
+        # find backlinks where OP and new
+        postSet = set()
+        for post in cur:
+            if post.get('com') and 'quotelink' in post['com']:
+                clean = yotsuba.escapeHtml(post['com'])
+                link = re.findall(r'(?<=>>)(\d+)', clean)
+                postSet.update(map(int, link))
+        threadQueue.update(postSet & expired - allThreads - {cur[0]['no']})
+        allThreads.update(threadQueue)
+        time.sleep(1)
+        if len(allThreads) > 5:
+            break
+
+    [print(i, ',', str(bucket[i])) for i in sorted(bucket)]
